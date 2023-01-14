ef4b306d9e72204a703e0c64afe1a11604902fa9
Ksenia Kriger <kriger.k.v@gmail.com>
2023-01-14 16:53:28 +0300

    внести правки первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,31 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__open-popup');
+const formElement = document.querySelector('.edit-profile');
+const nameInput = formElement.querySelector('.edit-profile__personal-data_input_name');
+const jobInput = formElement.querySelector('.edit-profile__personal-data_input_description');
+const profileName = document.querySelector('.profile__name')
+const profileDescription = document.querySelector('.profile__description')
 
 function openPopup() {
   popupElement.classList.add('popup_opened');
+  nameInput.textContent = profileName.value;
+  jobInput.textContent = profileDescription.value;
 }
 
 function closePopup() {
   popupElement.classList.remove('popup_opened');
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-
-popupCloseButtonElement.addEventListener('click', closePopup);
-
-const formElement = document.querySelector('.edit-profile');
-const nameInput = formElement.querySelector('.edit-profile__name');
-const jobInput = formElement.querySelector('.edit-profile__description');
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    const profileName = document.querySelector('.profile__name')
-    const profileDescription = document.querySelector('.profile__description')
 
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
 
+  closePopup();
 }
-handleFormSubmit
 
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
-formElement.addEventListener('submit', closePopup);
