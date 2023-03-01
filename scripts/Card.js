d7ecd6d601140da0b20d7d1b9823e77c60fd3f2e
KrigerK <116812368+KrigerK@users.noreply.github.com>
2023-03-01 19:16:44 +0300

    рефакторинг классы

---
+++ scripts/Card.js
@@ -0,0 +1,55 @@
+export default class Card {
+  constructor(data, templateSelector, handleOpenBigImage) {
+    this._name = data.name,
+    this._link = data.link,
+    this._templateSelector = templateSelector,
+    this._handleOpenBigImage = handleOpenBigImage
+}
+  _getTemplate(){
+    const cardElement = document
+    .querySelector(this._templateSelector)
+    .content
+    .querySelector('.card')
+    .cloneNode(true);
+
+    return cardElement;
+}
+
+  _toggleLikeElement(){
+    this._card.querySelector('.card__like').classList.toggle('card__like_active');
+  }
+
+  _removeCard(){
+    this._card.remove();
+  }
+
+  _setEventListeners() {
+
+    this._card.querySelector('.card__like').addEventListener('click', () => {
+      this._toggleLikeElement();
+    });
+
+    this._card.querySelector('.card__trash-btn').addEventListener('click', () => {
+      this._removeCard();
+    });
+    this._card.querySelector('.card__image').addEventListener('click',()=>{
+      this._handleOpenBigImage(this._link, this._name)
+    })
+  }
+
+  createNewCard(){
+    this._card = this._getTemplate();
+
+    this._card.querySelector('.card__image').src = this._link;
+    this._card.querySelector('.card__image').alt = this._name;
+    this._card.querySelector('.card__title').textContent = this._name;
+    this._setEventListeners();
+
+    return this._card;
+
+}
+
+}
+
+
+
---
+++ scripts/FormValidator.js
@@ -0,0 +1,52 @@
+export default class FormValidator {
+  constructor(config, form) {
+    this._config = config,
+    this._form = form
+  }
+
+  _handleFormInput = (event) => {
+    this._input = event.target;
+    this._inputId = this._input.id;
+    this._inputErrorElement = document.querySelector(`#${this._inputId}-error`);
+
+    if (!this._input.validity.valid) {
+      this._input.classList.add(this._config.inputErrorClass);
+      this._inputErrorElement.textContent = this._input.validationMessage;
+    } else {
+      this._input.classList.remove(this._config.inputErrorClass);
+      this._inputErrorElement.textContent = '';
+    }
+  }
+  _addInputListeners = () => {
+
+      this._form.addEventListener('input', (event) => {
+        this._handleFormInput(event);
+      });
+    };
+
+
+  _toggleButton = () => {
+    this._submitButton = this._form.querySelector(this._config.submitButtonSelector);
+    this._isFormValid = this._form.checkValidity();
+
+    this._submitButton.disabled = !this._isFormValid;
+    this._submitButton.classList.toggle(this._config.inactiveButtonClass, !this._isFormValid);
+  };
+
+  enableValidation = () => {
+
+      this._form.addEventListener('input', () => this._toggleButton());
+
+      this._addInputListeners();
+      this._toggleButton();
+
+      this._form.addEventListener('reset', () => {
+        setTimeout(() => {
+          this._toggleButton();
+        }, 0);
+      });
+  };
+
+}
+
+
